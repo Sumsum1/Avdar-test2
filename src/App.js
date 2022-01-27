9f1c6076c3f95400d5f195124744edee86aed47b
Asaf <asaf.l@visual3d.co.il>
2022-01-27 13:26:04 +0200

    deployment with video

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import image from './screen-1.jpg'
+import movie from './profile.webm';
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState(0);
+  const [stop1, setSop1]  =useState(false);
+  const [reverse, setReverse] = useState(false);
+  const [forward, setForward] = useState(true);
+  const [play1, setPlay1] = useState(false);
+  const movieRef = useRef();
+
+  const onTimeUpdate = (e) => {
+    const c = movieRef.current.currentTime;
+    movieRef.current.currentTime = c + 0.104;
+}
+
   return (
-    <div className="App">
-        <h1>Heroku deployment</h1>
-        <img src={image}/>
-    </div>
+    <div className='container-video'>
+          <video 
+            className='video-room' 
+            onWheel={onTimeUpdate}  
+            muted={true} 
+            ref={movieRef} 
+            src={movie} 
+            //onTimeUpdate={onTimeUpdate}   
+      />
+      </div>
   );
 }
 
